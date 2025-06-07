eab1f70868b8a07ff91a72972a449fdfda9afea0
Elenaaaak <kekicelena@gmail.com>
2025-06-07 12:41:02 +0200

    pushing more stuff in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FriendGameComponent } from './components/friend-game/friend-game.component';
+import { AIGameComponent } from './components/ai-game/ai-game.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'friend-game',
+    component: FriendGameComponent,
+  },
+  {
+    path: 'AI-game',
+    component: AIGameComponent,
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FriendGameComponent,
+    AIGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
