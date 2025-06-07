7227851b5942a0fb06210b75f86f8ec652078dda
Elenaaaak <kekicelena@gmail.com>
2025-06-07 12:36:38 +0200

    Initial commit with the homepage ready

---
+++ src/app/components/ai-game/ai-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-game',
+  templateUrl: './ai-game.component.html',
+  styleUrls: ['./ai-game.component.scss']
+})
+export class AIGameComponent {
+
+}
---
+++ src/app/components/friend-game/friend-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-game',
+  templateUrl: './friend-game.component.html',
+  styleUrls: ['./friend-game.component.scss']
+})
+export class FriendGameComponent {
+
+}
